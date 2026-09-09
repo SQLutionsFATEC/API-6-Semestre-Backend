39cea3a3ad6b3134066100f0b43bfd92f4385e33
EnzoLFranco <fenzolemos@gmail.com>
2026-09-09 17:36:12 -0300

    feat(21): Refactor documento_api to use DocumentoViewSet for improved API structure

--- django/api/views/documento_api.py
+++ django/api/views/documento_api.py
@@ -1,29 +1,9 @@
-from django.http import JsonResponse
-from django.views.decorators.http import require_GET
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
 from api.models import Documento
+from api.serializers import DocumentoSerializer
 
 
-@require_GET
-def documento_api(request, id_documento=None):
-	if id_documento is None or not id_documento.isdecimal():
-		return JsonResponse(
-			{'erro': 'id_documento deve ser um número inteiro válido.'},
-			status=400,
-		)
-
-	documento = Documento.objects.filter(id_documento=int(id_documento)).first()
-	if documento is None:
-		return JsonResponse(
-			{'erro': 'Documento não encontrado.'},
-			status=404,
-		)
-
-	return JsonResponse({
-		'id_documento': documento.id_documento,
-		'tipo_arquivo': documento.tipo_arquivo,
-		'nome': documento.nome,
-		'setor': documento.setor,
-		'data_atualizacao': documento.data_atualizacao.isoformat(),
-		'nivel': documento.nivel,
-		'data': documento.data.name,
-	})
+class DocumentoViewSet(ReadOnlyModelViewSet):
+	queryset = Documento.objects.all()
+	serializer_class = DocumentoSerializer
