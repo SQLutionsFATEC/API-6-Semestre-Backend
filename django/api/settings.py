d17e95e2e5497c7b3b06262fb1a0d96d4a05eda0
Elbert Jean <elbertjeandossantos@gmail.com>
2026-09-09 21:36:44 -0300

    fix(7): resolve docker startup and django configuration issues

--- django/api/settings.py
+++ django/api/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR.parent / '.env')
+load_dotenv(BASE_DIR.parent / 'deploy' / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     'django_prometheus',
     'django_seed',
     'corsheaders',
