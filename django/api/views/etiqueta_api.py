1e03c9a0541c3170b293a6e51008555a40064340
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2026-09-16 14:29:58 -0300

    style(22): fix views/etiqueta_api indentation

--- django/api/views/etiqueta_api.py
+++ django/api/views/etiqueta_api.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from api.serializers import EtiquetaSerializer
 from api.models import Etiqueta
 
+
 class EtiquetaViewSet(ModelViewSet):
-	queryset = Etiqueta.objects.all()
-	serializer_class = EtiquetaSerializer
-		
+    queryset = Etiqueta.objects.all()
+    serializer_class = EtiquetaSerializer
