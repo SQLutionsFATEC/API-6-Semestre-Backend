edb877e9e04a3d349a74cae90750325f7c1bccf3
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2026-09-23 09:49:10 -0300

    build(14): add vector extension to sonar

--- deploy/postgres/init.sh
+++ deploy/postgres/init.sh
@@ -6,3 +6,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE EXTENSION IF NOT EXISTS vector;
     ALTER ROLE "$POSTGRES_USER" CREATEDB;
 EOSQL
+
+# Mantém a extensão disponível também em bancos de teste criados a partir do template.
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname template1 <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS vector;
+EOSQL
