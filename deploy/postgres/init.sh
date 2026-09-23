9e88e438b07f4bf38fa533066152374512f3870d
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2026-09-23 09:42:19 -0300

    build(14): add pgvector to requiments and dockerfile from backend

--- deploy/postgres/init.sh
+++ deploy/postgres/init.sh
@@ -3,5 +3,6 @@ set -e
 
 # Permite que o usuário da aplicação crie o banco de testes do Django.
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS vector;
     ALTER ROLE "$POSTGRES_USER" CREATEDB;
 EOSQL
